0120b4f79ded1dec42ca7b0e98a0d854ec4f988e
neuspeed <neuspeed@users.noreply.github.com>
2024-09-04 20:25:28 +0000

    ✅ Ready to clone and code.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import sys
 import pytest
 
-from project_name import create_app
-from project_name.ext.commands import populate_db
-from project_name.ext.database import db
+from test import create_app
+from test.ext.commands import populate_db
+from test.ext.database import db
 
 
 @pytest.fixture(scope="session")
